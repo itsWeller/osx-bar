03d97c45b2e0da058f73ec9eae2f02b84654ad79
Chris Weller <chriskweller@gmail.com>
2015-05-26 20:29:50 -0700

    Initial commit.

---
+++ vanilla.py
@@ -0,0 +1,44 @@
+#! /usr/bin/python
+from mpd import MPDClient
+from time import strftime
+import os
+
+title_bar = [' ' for _ in xrange(197)] # Magical number for Geektool window length
+
+left    = []
+center  = []
+right   = []
+
+def build_segment(segment, data):
+    segment.extend(data)
+
+def build_bar(left=left, center=center, right=right, title_bar=title_bar):
+    mid_points = (len(title_bar)/2-len(center)/2, len(title_bar)/2+len(center)/2)
+
+    title_bar[:len(left)] = left
+    title_bar[mid_points[0]:mid_points[1]] = center
+    title_bar[len(title_bar) - len(right):] = right
+
+# Music Info
+client = MPDClient()
+client.connect("localhost",6600)
+song_info = client.currentsong()
+
+# Time
+now = str(strftime("%I:%M"))
+
+# Default Hostname (Yes I know I should get it programatically, but, lazy.
+hostname = " chris@air.local"
+
+# Battery left
+battery = os.popen("pmset -g batt " + "|" + 
+                   "egrep '([0-9]+\%).*' -o --colour=auto " + "|" +
+                   "cut -f1 -d';'").read().strip()
+
+build_segment(left, " chris@air.local")
+build_segment(center, song_info['artist']+' // '+song_info['title'])
+build_segment(right, battery + " | " + now + " ")
+
+build_bar()
+
+print ''.join(title_bar)
